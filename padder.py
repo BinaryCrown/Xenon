a4ecad7e0c3550953af29dcb304ca8d792d239d2
Jayde 3 <93054701+BinaryCrown@users.noreply.github.com>
2024-05-15 11:45:37 +0000

    Optimized Hello World and fixed padder

--- padder.py
+++ padder.py
@@ -15,8 +15,8 @@ chunks = [string[8*n:8*n+8] for n in range(len(string)//8)]
 out = []
 for e in chunks:
     hx = hex(int(e,2))[2:]
-    if len(hx) == 1:
-        hx = "0" + hx
+    if len(hx) < 2:
+        hx = "0"*(2-len(hx)) + hx
     hx = bytes.fromhex(hx).decode("latin-1")
     out.append(hx)
 path = input("Place to store XEN: ")
